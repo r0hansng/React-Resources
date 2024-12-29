fd8aab1ec23035c54fb4d54f6f31e34777aa0241
rohansingh <rohan.singh.en@gmail.com>
2024-12-29 20:49:06 +0530

    The useState() Hook

---
+++ Hooks/01_useState()/src/App.jsx
@@ -0,0 +1,14 @@
+// useState() = A react hook that allows the creation of a stateful variable AND a setter function to update its value in the virtual DOM. [name, setName]
+
+import MyComponent from "./MyComponent"
+
+function App() {
+
+  return(
+    <>
+      <MyComponent />
+    </>
+  )
+}
+
+export default App
