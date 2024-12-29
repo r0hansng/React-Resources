10ccd6bfd0701098aa4c398b260bd67325d4cd13
rohansingh <rohan.singh.en@gmail.com>
2024-12-29 20:46:39 +0530

    How to create Card Components

---
+++ 02_card_compenents/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile-picture" />
+            <h2 className="card-title">Rohan Singh</h2>
+            <p className="card-text">I am a computer science student</p>
+        </div>
+    )
+}
+
+export default Card
